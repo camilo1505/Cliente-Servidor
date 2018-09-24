f63506452133e521d19b683c6909aae9408fadea
Cristian Camilo Giraldo <cristian_camilo1505@hotmail.com>
2018-09-24 15:54:31 -0500

    producto punto c

---
+++ Matrix/productoPunto.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<limits>
+
+using namespace std;
+const int inf = numeric_limits<int>::max();
+
+int main(int argc, char** argv)
+{
+    int *m1, *m2, *m3;
+    int tamano = 3;
+
+    m1 = (int*) malloc(tamano*tamano*sizeof(int));
+    m2 = (int*) malloc(tamano*tamano*sizeof(int));
+    m3 = (int*) malloc(tamano*tamano*sizeof(int));
+
+    
+
+}
+
