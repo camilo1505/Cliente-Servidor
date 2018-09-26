dca01e49ee86840b8999e44fe5819731410bbc8a
Cristian Camilo Giraldo <cristian_camilo1505@hotmail.com>
2018-09-26 15:56:39 -0500

    funciona completamente

--- Tarea 3/productoPunto.cpp
+++ Tarea 3/productoPunto.cpp
@@ -14,7 +14,14 @@ void printMatrix(int (&matrix)[matrixSize][matrixSize])
         printf("[");
         for(int j=0; j < matrixSize; j++)
         {
-            printf("%i ", matrix[i][j]);
+            if(matrix[i][j] == inf)
+            {
+                printf("inf \t");
+            }
+            else
+            {
+                printf("%i \t", matrix[i][j]);                
+            }
         }
         printf("]\n");
     }
@@ -22,43 +29,59 @@ void printMatrix(int (&matrix)[matrixSize][matrixSize])
 
 void point(int (&m1)[matrixSize][matrixSize], int (&m2)[matrixSize][matrixSize], int (&m3)[matrixSize][matrixSize])
 {
-    for(int row = 0; row < matrixSize; row++)
+    for(int i = 0; i < matrixSize; i++)
     {
-        for(int colA = 0; colA < matrixSize; colA++)
+        for(int j = 0; j < matrixSize; j++)
         {
-            for(int colB = 0; colB < matrixSize; colB++)
+            for(int k = 0; k < matrixSize; k++)
             {
-                m3[row][colA] += m1[row][colB] * m2[colB][colA];
+                m3[i][j] += m1[i][k] * m2[k][j];
             }
         }
     }
 }
 
+int suma(int a, int b)
+{
+    if(a == inf)
+    {
+        return a;
+    }
+    if(b == inf)
+    {
+        return b;
+    }
+    else
+    {
+        return a+b;
+    }
+}
+
 void pointStrange(int (&m1)[matrixSize][matrixSize], int (&m2)[matrixSize][matrixSize], int (&m3)[matrixSize][matrixSize])
 {
-    //printMatrix(m1);
-    //printf("\n\n|||||||||||||||||||||||||||||||||||||||||||\n\n");
-    //printMatrix(m2);
-    //printf("\n\n|||||||||||||||||||||||||||||||||||||||||||\n\n");
-    //printMatrix(m3);
-    for(int row = 0; row < matrixSize; row++)
+    for(int i = 0; i < matrixSize; i++)
     {
-        for(int colA = 0; colA < matrixSize; colA++)
+        for(int j = 0; j < matrixSize; j++)
         {
-            for(int colB = 0; colB < matrixSize; colB++)
+            for(int k = 0; k < matrixSize; k++)
             {
-                //printf("m3[%i][%i] = %i\n", row, colA, m3[row][colA]);
-                //printf("min(%i, %i) = %i\n", m3[row][colA], m1[row][colB] + m2[colB][colA], std::min(m3[row][colA], m1[row][colB] + m2[colB][colA]));
-                if(m1[row][colB] == inf and m2[colB][colA] == inf)
-                {
-                    m3[row][colA] = std::min(m3[row][colA], inf);
-                }
-                else
-                {
-                    m3[row][colA] = std::min(m3[row][colA], m1[row][colB] + m2[colB][colA]);
-                }
-                
+                m3[i][j] = std::min( m3[i][j], suma(m1[i][k],m2[k][j]) );          
             }
+        }
+    }
+}
+
+void powStrange(int (&m1)[matrixSize][matrixSize], int (&matrixResul)[matrixSize][matrixSize], int pot)
+{
+    for(int i = 1; i <= pot; i++)
+    {
+        if(i == 1)
+        {
+            pointStrange(m1, m1, matrixResul);
+        }
+        else
+        {
+            pointStrange(matrixResul, m1, matrixResul);
         }
     }
 }
@@ -67,14 +90,14 @@ int main()
 {
     printf("Valor INF = %i \n", inf);
     int matrix[matrixSize][matrixSize]= { 
-        0,inf,5,inf,inf,inf,inf,inf,
-        inf,0,8,inf,inf,inf,8,inf,
-        inf,inf,0,inf,6,inf,inf,inf,
-        inf,5,inf,0,inf,inf,inf,inf,
-        inf,inf,1,inf,0,inf,1,8,
-        inf,inf,inf,inf,inf,0,inf,inf,
-        inf,inf,inf,inf,4,inf,0,inf,
-        inf,inf,inf,inf,inf,inf,1,0
+        0,6,inf,inf,inf,2,inf,inf,
+        inf,0,inf,8,inf,2,inf,inf,
+        1,inf,0,inf,inf,inf,inf,inf,
+        inf,8,inf,0,inf,inf,inf,inf,
+        inf,inf,inf,inf,0,inf,inf,inf,
+        inf,inf,inf,inf,inf,0,3,inf,
+        inf,inf,inf,inf,inf,inf,0,4,
+        inf,inf,inf,inf,inf,5,inf,0
     };
 
     int matrixResul[matrixSize][matrixSize]= {
@@ -88,7 +111,8 @@ int main()
         inf,inf,inf,inf,inf,inf,inf,inf
     };
 
-    pointStrange(matrix, matrix, matrixResul);
+    //pointStrange(matrix, matrix, matrixResul);
+    powStrange(matrix, matrixResul, matrixSize);
     printMatrix(matrixResul);
 
     return 0;
